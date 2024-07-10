b466cf53cd374f2f74cbffd8162268465bea0191
Aden Mariyappa <46941201+AdenMar@users.noreply.github.com>
2024-07-10 15:38:18 -0400

    Init Publish

---
+++ src/main/java/net/robin/compactor/ClientCompactor.java
@@ -0,0 +1,11 @@
+package net.robin.compactor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientCompactor implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
